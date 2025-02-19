ec427bc1896f301e063cfad7e1cf35e175b289c0
JorisAlbeda <91875360+JorisAlbeda@users.noreply.github.com>
2025-02-19 11:32:04 +0100

    Added index page.

--- app.vue
+++ app.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     <SiteHeader />
-    <h1>Hello, Nuxt 3!</h1>
+    <main class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+      <NuxtPage />
+    </main>
   </div>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <section>
+    <h1 class="text-5xl font-bold mt-20">Hello, I'm Joris Albeda!</h1>
+    <p class="text-base text-gray-900 p-2 italic">
+      Frontend developer with great dreams.
+    </p>
+  </section>
+  <section class="flex flex-col md:flex-row">
+    <div class="md:w-3/4">
+      <h2 class="text-3xl font-bold mt-8">My story</h2>
+      <p class="text-lg py-2">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis non
+        numquam eaque in delectus explicabo hic, distinctio rerum officia ullam
+        quod magni vero velit repellendus beatae, tempore sint minima nobis!
+      </p>
+      <p class="text-lg py-2">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic, nihil
+        maxime facere sed laboriosam temporibus aliquid minima dolore modi
+        repudiandae commodi quae beatae nulla obcaecati ut, aspernatur
+        doloremque? Veniam, debitis.
+      </p>
+    </div>
+
+    <img
+      src="@/assets/images/Joris.jpg"
+      alt="Joris"
+      class="w-1/2 md:max-w-sm p-8 mx-auto"
+    />
+  </section>
+
+  <section>
+    <h2 class="text-3xl font-bold mt-8">Latest Blog Posts</h2>
+  </section>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
