2f3a7c04c55e6f609576dfd994271f4a61d1b637
JorisAlbeda <91875360+JorisAlbeda@users.noreply.github.com>
2025-02-19 13:40:48 +0100

    Added Nuxt Content.

---
+++ pages/blog/[...slug].vue
@@ -0,0 +1,12 @@
+<script setup>
+const slug = useRoute().params.slug
+const { data: post } = await useAsyncData(`blog-${slug}`, () => {
+  return queryCollection("content").path(`/blog/${slug}`).first()
+})
+</script>
+
+<template>
+  <!-- Render the blog post as Prose & Vue components -->
+  <ContentRenderer v-if="post" :value="post" />
+  <div v-else>Post {{ slug }} not found</div>
+</template>
