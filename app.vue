4f277d9f287ceffae1e9f8901d9c3680871551f3
JorisAlbeda <91875360+JorisAlbeda@users.noreply.github.com>
2025-02-27 13:42:33 +0100

    Added tags to blog page.

--- app.vue
+++ app.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-col min-h-screen">
     <SiteHeader />
     <main
-      class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 w-full grow items-start"
+      class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 w-full grow items-start bg-slate-300"
     >
       <NuxtPage />
     </main>
--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -7,6 +7,21 @@ const { data: post } = await useAsyncData(`blog-${slug}`, () => {
 
 <template>
   <!-- Render the blog post as Prose & Vue components -->
-  <ContentRenderer class="prose" v-if="post" :value="post" />
+  <ContentRenderer
+    class="prose my-10 mx-auto max-w-7xl"
+    v-if="post"
+    :value="post"
+  />
   <div v-else>Post {{ slug }} not found</div>
+  <div class="my-8">
+    <a
+      v-for="tag in post.meta.tags"
+      :key="tag"
+      :href="`/blog/tags/${tag}`"
+      class="text-sm font-semibold inline-block py-2 px-4 rounded-lg text-gray-100 bg-blue-500 uppercase last:mr-0 mr-4"
+    >
+      <Icon name="entypo:tag" size="1.5rem" class="text-gray-100 mr-2"></Icon>
+      {{ tag }}
+    </a>
+  </div>
 </template>
